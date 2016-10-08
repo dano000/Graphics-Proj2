bb08a381b72ace38030f3042a955244ecb6b6e4e
dano000 <me@schulzd.me>
2016-10-08 13:50:17 +1100

    playing sounds at start of game

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class gameController : MonoBehaviour {
 
     public GameObject[] sounds;
+    public GameObject[] instanciatedSounds;
     public float spacing = 5.0f;
     public GameObject[] soundOrder;
     public int[] soundOrderRotation;
@@ -11,7 +12,10 @@ public class gameController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         generateObjects(sounds);
-	}
+        generateSoundOrder(1);
+        StartCoroutine(playEngineSound());
+    }
+     
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,32 +28,40 @@ public class gameController : MonoBehaviour {
         {
             Vector3 currPos = new Vector3(-spacing + (i* spacing), 0, 0);
             Quaternion currRot = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
-            Instantiate(soundObjectArray[i],currPos,currRot);
+            GameObject s = (GameObject)Instantiate(soundObjectArray[i], currPos, currRot);
+            instanciatedSounds[i] = s;
         }
+
     }
 
     void generateSoundOrder(int level)
     {
         for(int i=0; i< level; i++)
         {
-            GameObject selectedSound = sounds[Random.Range(0,sounds.Length)];
-            for(int j=0; j < Random.Range(0, maxRepeats);j++)
+            GameObject selectedSound = instanciatedSounds[Random.Range(0,sounds.Length)];
+            for(int j=0; j < Random.Range(1, maxRepeats);j++)
             {
+                Debug.Log(j);
                 soundOrder[j] = selectedSound;
                 soundOrderRotation[j] = 1;
             }
         }
     }
 
-    void playSoundOrder()
+
+    // Source: http://answers.unity3d.com/questions/904981/how-to-play-an-audio-file-after-another-finishes.html
+    IEnumerator playEngineSound()
     {
-        for(int i=0;i <soundOrder.Length; i++)
+        for (int i = 0; i < soundOrder.Length; i++)
         {
-            AudioSource currentAudio = soundOrder[i].GetComponent<AudioSource>();
+            AudioSource currentAudio = soundOrder[i].GetComponentInChildren<AudioSource>();
             
             currentAudio.pitch = soundOrderRotation[i];
             currentAudio.Play();
-            //WaitForSeconds(currentAudio.clip.length);
+            yield return new WaitForSeconds(currentAudio.clip.length);
         }
     }
+
+
+        
 }
